b8afa4c3c0dab51c06cfe2bee47bc605a1a92036
siddharth <siddharth.srivastava@tothenew.com>
2025-03-25 17:17:37 +0530

    npm update

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["your-secret-key-1", "your-secret-key-2"]),
   },
 });
